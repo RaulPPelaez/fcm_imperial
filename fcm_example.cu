7cb52379756154bb57308cfdf85f2fad0a41c1e7
RaulPPealez <raul.perez@uam.es>
2022-09-14 13:09:31 +0200

    Update

--- fcm_example.cu
+++ fcm_example.cu
@@ -5,9 +5,9 @@
 
 
 using real = uammd_fcm::real;
-template<class T>using gpu_container = uammd_fcm::cached_vector<T>;
 
 int main(){
+  //Initialize
   real hydrodynamicRadius = 1.0;
   real viscosity = 1/(6*M_PI);
   real Lx,Ly,Lz;
@@ -15,25 +15,32 @@ int main(){
   real tolerance = 1e-4;
   auto fcm = uammd_fcm::initializeFCM(hydrodynamicRadius, viscosity, Lx, Ly, Lz, tolerance);
 
-  int i_numberParticles = 100;
-  gpu_container<real> i_pos(3*i_numberParticles);
+  //One single particle at the origin
+  int i_numberParticles = 1;
+  thrust::device_vector<real> i_pos(3*i_numberParticles);
   thrust::fill(i_pos.begin(), i_pos.end(), 0);
-  int o_numberParticles = 10;
-  gpu_container<real> o_pos(3*o_numberParticles);
+  int o_numberParticles = 1;
+  thrust::device_vector<real> o_pos(3*o_numberParticles);
   thrust::fill(o_pos.begin(), o_pos.end(), 0);
-  gpu_container<real> forceTorque(2*3*i_numberParticles);
+  thrust::device_vector<real> forceTorque(2*3*i_numberParticles);
   thrust::fill(forceTorque.begin(), forceTorque.end(), 0);
+
+  forceTorque[0] = 1; //A force on the X direction
+
 
   auto i_pos_ptr = thrust::raw_pointer_cast(i_pos.data());
   auto o_pos_ptr = thrust::raw_pointer_cast(o_pos.data());
   auto ft_ptr = thrust::raw_pointer_cast(forceTorque.data());
-
   auto result = uammd_fcm::computeHydrodynamicDisplacements(fcm,
 							    i_pos_ptr, ft_ptr, i_numberParticles,
 							    o_pos_ptr, o_numberParticles);
 
   auto monopoles = result.first;
   auto dipoles = result.second;
+  thrust::host_vector<uammd::real3> h_monopoles(result.first.begin(), result.first.end());
+  thrust::host_vector<uammd::real3> h_dipoles(result.second.begin(), result.second.end());
+
+  std::cerr<<h_monopoles[0]<<std::endl;
 
 
   return 0;
